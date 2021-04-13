7dc0d7771fde43889ab4e4c21a44ad367b3b4be9
Pratiksha0401 <nagoshepratiksha@gmail.com>
2021-04-13 20:27:11 +0530

    Added : code for comparison between two lines using equals method

--- line_comparison.java
+++ line_comparison.java
@@ -4,14 +4,30 @@ class line_comparison
 	public static void main(String[] args) 
 	{
 		Scanner sc=new Scanner(System.in);
+
+		System.out.println("Enter co-ordinates for 1st line  ");
 		System.out.println("Enter x1 and y1 co-ordinates for 1st point:  ");
 		int x1=sc.nextInt();
 		int y1=sc.nextInt();
 		System.out.println("Enter x2 and y2 co-ordinates for 2nd point:  ");
 		int x2=sc.nextInt();
 		int y2=sc.nextInt();
-		float length;
-		length=(float)Math.sqrt(Math.pow(x2-x1,2)+Math.pow(y2-y1,2));
-		System.out.println("Length is " + length);		
+		float length_L1;
+		length_L1=(float)Math.sqrt(Math.pow(x2-x1,2)+Math.pow(y2-y1,2));
+		String L1=String.valueOf(length_L1);    
+		System.out.println("Length is " + length_L1+ '\n');	
+		
+		System.out.println("Enter co-ordinates for 1st line  ");
+		System.out.println("Enter x3 and y3 co-ordinates for 1st point:  ");
+		int x3=sc.nextInt();
+		int y3=sc.nextInt();
+		System.out.println("Enter x4 and y4 co-ordinates for 2nd point:  ");
+		int x4=sc.nextInt();
+		int y4=sc.nextInt();
+		float length_L2;
+		length_L2=(float)Math.sqrt(Math.pow(x4-x3,2)+Math.pow(y4-y3,2));
+		String L2=String.valueOf(length_L2);   
+		System.out.println("Length is " + length_L2  + '\n');
+        System.out.println("Lenght of line 1 and line 2  isEquals "+L1.equals(L2));
 	}
 }
