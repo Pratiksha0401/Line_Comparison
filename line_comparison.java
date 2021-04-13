bde6f7c8692c8ccb19fa6a21ae4628e3b6ca7b54
Pratiksha0401 <nagoshepratiksha@gmail.com>
2021-04-13 19:58:39 +0530

    Length between two points

--- line_comparison.java
+++ line_comparison.java
@@ -1,7 +1,17 @@
+import java.util.*;
 class line_comparison 
 {
 	public static void main(String[] args) 
 	{
-		System.out.println("Welcome to Line comparison_");
+		Scanner sc=new Scanner(System.in);
+		System.out.println("Enter x1 and y1 co-ordinates for 1st point:  ");
+		int x1=sc.nextInt();
+		int y1=sc.nextInt();
+		System.out.println("Enter x2 and y2 co-ordinates for 2nd point:  ");
+		int x2=sc.nextInt();
+		int y2=sc.nextInt();
+		float length;
+		length=(float)Math.sqrt(Math.pow(x2-x1,2)+Math.pow(y2-y1,2));
+		System.out.println("Length is " + length);		
 	}
 }
