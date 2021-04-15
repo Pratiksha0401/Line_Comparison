5f7d2ead598036122257bc301b2c5456426d6c76
Pratiksha0401 <nagoshepratiksha@gmail.com>
2021-04-15 16:37:04 +0530

    Refactor: code for comparison between two lines using equals method using OOPs concept

--- line_comparison.java
+++ line_comparison.java
@@ -9,9 +9,9 @@ class Co_ordinates
 
   float length;
 
-  void setCo_ordinates()
+  void setCo_ordinates(int line_number)
   {
-	System.out.println("Enter co-ordinates for line ");
+	System.out.println("Enter co-ordinates for line "+line_number);
 	Scanner sc=new Scanner(System.in);
 	System.out.println("Enter 1st point(x1,y1) : ");
 	x1=sc.nextInt();
@@ -27,13 +27,23 @@ class Co_ordinates
     return length;
   }
 }
+
 class line_comparison
 {
 	public static void main(String[] args) 
 	{
-	Co_ordinates line1=new Co_ordinates();
-    line1.setCo_ordinates();
+		Co_ordinates line1=new Co_ordinates();
+    line1.setCo_ordinates(1);
     float length_line1=line1.get_length();
-    System.out.println("Length of line is " + length_line1+'\n');
-  }
-} 
+    System.out.println("Length Line 1 is " + length_line1+'\n');
+    String length_l=String.valueOf(length_line1); 
+
+    Co_ordinates line2=new Co_ordinates();
+    line2.setCo_ordinates(2);
+    float length_line2=line2.get_length();
+    System.out.println("Length Line 2 is " + length_line2+'\n');
+    String length_2=String.valueOf(length_line2); 
+
+        System.out.println("Lenght of line 1 and line 2  isEquals "+length_l.equals(length_2));
+	}
+}
